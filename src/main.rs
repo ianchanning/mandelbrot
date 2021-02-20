61d4f9034b79c092b2946862765ac01fadc26156
Ian Channing <ian@ianchanning.com>
2021-02-20 13:10:46 +0100

    add complex num struct comment

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 extern crate num;
 use num::Complex;
 
+/* Basis of the Complex number struct from the num crate
+struct Complex<T> {
+    /// Real portion of the complex number
+    re: T,
+    /// Imaginary portion of the complex number
+    im: T
+}
+*/
+
 #[allow(dead_code)]
 fn complex_square_add_loop(c: Complex<f64>) {
     let mut z = Complex { re: 0.0, im: 0.0 };
