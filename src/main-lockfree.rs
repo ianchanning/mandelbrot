40a4bf480809d5197e4b28881708e49ba52de8cc
Ian Channing <ian@ianchanning.com>
2021-03-14 16:56:30 +0100

    add alternative concurrency strategies

--- src/main-lockfree.rs
+++ src/main-lockfree.rs
@@ -22,11 +22,8 @@ struct Complex<T> {
 /// return `None`.
 fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     let mut z = Complex { re: 0.0, im: 0.0 };
-    // let mut z = c;
-
     for i in 0..limit {
-        z = z*z + c;
-        // z = z*z;
+        z = z * z + c;
         if z.norm_sqr() > 4.0 {
             return Some(i);
         }
@@ -80,7 +77,7 @@ fn parse_complex(s: &str) -> Option<Complex<f64>> {
 #[test]
 fn test_parse_complex() {
     assert_eq!(parse_complex("1.25,-0.0625"),
-    Some(Complex { re: 1.25, im: -0.0625 }));
+               Some(Complex { re: 1.25, im: -0.0625 }));
     assert_eq!(parse_complex(",-0.0625"), None);
 }
 
@@ -100,19 +97,19 @@ fn pixel_to_point(bounds: (usize, usize),
     let (width, height) = (lower_right.re - upper_left.re,
                            upper_left.im - lower_right.im);
     Complex {
-        re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+        re: upper_left.re + pixel.0 as f64 * width  / bounds.0 as f64,
         im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
-            // Why subtraction here? pixel.1 increases as we go down,
-            // but the imaginary component increases as we go up.
+        // Why subtraction here? pixel.1 increases as we go down,
+        // but the imaginary component increases as we go up.
     }
 }
 
 #[test]
 fn test_pixel_to_point() {
     assert_eq!(pixel_to_point((100, 100), (25, 75),
-    Complex { re: -1.0, im: 1.0 },
-    Complex { re: 1.0, im: -1.0 }),
-    Complex { re: -0.5, im: -0.5 });
+                              Complex { re: -1.0, im:  1.0 },
+                              Complex { re:  1.0, im: -1.0 }),
+               Complex { re: -0.5, im: -0.5 });
 }
 
 /// Render a rectangle of the Mandelbrot set into a buffer of pixels.
@@ -131,7 +128,7 @@ fn render(pixels: &mut [u8],
     for row in 0 .. bounds.1 {
         for column in 0 .. bounds.0 {
             let point = pixel_to_point(bounds, (column, row),
-            upper_left, lower_right);
+                                       upper_left, lower_right);
             pixels[row * bounds.0 + column] =
                 match escape_time(point, 255) {
                     None => 0,
@@ -160,7 +157,7 @@ fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize))
     Ok(())
 }
 
-
+use atomic_chunks_mut::AtomicChunksMut;
 use std::io::Write;
 
 fn main() {
@@ -168,11 +165,11 @@ fn main() {
 
     if args.len() != 5 {
         writeln!(std::io::stderr(),
-                "Usage: mandelbrot FILE PIXELS UPPERLEFT LOWERRIGHT")
+                 "Usage: mandelbrot FILE PIXELS UPPERLEFT LOWERRIGHT")
             .unwrap();
         writeln!(std::io::stderr(),
-                "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
-                args[0])
+                 "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+                 args[0])
             .unwrap();
         std::process::exit(1);
     }
@@ -186,7 +183,28 @@ fn main() {
 
     let mut pixels = vec![0; bounds.0 * bounds.1];
 
-    render(&mut pixels, bounds, upper_left, lower_right);
+    let threads = 4;
+    let rows_per_band = bounds.1 / 400;
+
+    {
+        let bands = AtomicChunksMut::new(&mut pixels, rows_per_band * bounds.0);
+        crossbeam::scope(|spawner| {
+            for _ in 0..threads {
+                spawner.spawn(|| {
+                    for (i, band) in &bands {
+                        let top = i;
+                        let height = band.len() / bounds.0;
+                        let band_bounds = (bounds.0, height);
+                        let band_upper_left = pixel_to_point(bounds, (0, top),
+                                                             upper_left, lower_right);
+                        let band_lower_right = pixel_to_point(bounds, (bounds.0, top + height),
+                                                              upper_left, lower_right);
+                        render(band, band_bounds, band_upper_left, band_lower_right);
+                    }
+                });
+            }
+        });
+    }
 
     write_image(&args[1], &pixels, bounds)
         .expect("error writing PNG file");
